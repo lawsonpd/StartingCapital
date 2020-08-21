68280ed044d097d1cf25ed1be92c46dc00522cc1
twbrodarick <tbrodarick@gmail.com>
2020-08-20 21:56:03 -0500

    working app Thursday class

--- starting-capital-frontend/src/components/Contract/Contract.jsx
+++ starting-capital-frontend/src/components/Contract/Contract.jsx
@@ -2,6 +2,9 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Web3Service from '../../web3/web3.service';
 //import { Test } from './Contract.styles';
+import keys from "../../keys"
+import * as web3 from "web3"
+const Web3EthContract = require('web3-eth-contract');
 
 class Contract extends PureComponent { 
   constructor(props) {
@@ -14,12 +17,73 @@ class Contract extends PureComponent {
 
     this.web3 = null;
     this.setBalance = this.setBalance.bind(this);
+    this.abi = [
+      {
+          "inputs": [
+              {
+                  "internalType": "string",
+                  "name": "name",
+                  "type": "string"
+              },
+              {
+                  "internalType": "string",
+                  "name": "symbol",
+                  "type": "string"
+              },
+              {
+                  "internalType": "address payable",
+                  "name": "wallet",
+                  "type": "address"
+              }
+          ],
+          "payable": false,
+          "stateMutability": "nonpayable",
+          "type": "constructor"
+      },
+      {
+          "constant": true,
+          "inputs": [],
+          "name": "sct_sale_address",
+          "outputs": [
+              {
+                  "internalType": "address",
+                  "name": "",
+                  "type": "address"
+              }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+      },
+      {
+          "constant": true,
+          "inputs": [],
+          "name": "token_address",
+          "outputs": [
+              {
+                  "internalType": "address",
+                  "name": "",
+                  "type": "address"
+              }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+      }
+  ]
+  
+  
+  
+
   }
 
   componentWillMount = () => {
      //constructing an instance of web 3 with preset config
      this.web3 = new Web3Service();
-     this.setBalance()
+     const contract = new Web3EthContract(this.abi, keys.address)
+     console.log(contract)
+     let address = contract.methods.sct_sale_address().call()
+     console.log(address)
   }
 
   setBalance(){
