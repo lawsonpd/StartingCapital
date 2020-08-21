4263150dbb37e5f4b69d36109390d765cdcbf034
mdelgado <migueldelgadoangelo@gmail.com>
2020-08-21 09:24:57 -0500

    add logic for fetching second contract and create placeholder ui button to buy tokens

--- starting-capital-frontend/src/components/Contract/Contract.jsx
+++ starting-capital-frontend/src/components/Contract/Contract.jsx
@@ -1,48 +1,97 @@
 import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import Web3Service from '../../web3/web3.service';
 import * as Web3 from 'web3';
 import abi from './abi';
+import startingCapitalAbi from './startingCapitalAbi';
 import keys from "../../keys"
+import TextField from '@material-ui/core/TextField';
+import { Button, FormControl, InputLabel, OutlinedInput, InputAdornment, IconButton } from '@material-ui/core';
 
-
-class Contract extends PureComponent { 
+//TEST CONTRACT ADDRESS, ADD TO keys.js file
+//0x2BBBea5fA645865Fc61B3E09691DFC6157C5d9eF
+class Contract extends PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
-      hasError: false,
-      balance : 0,
-      sctSaleAddress : null
+      error: { exists: false, msg: '' },
+      balance: 0,
+      sctSaleAddress: null,
+      loading: true
     };
 
-    this.web3 = null;
-    this.setBalance = this.setBalance.bind(this);
+    this.web3 = new Web3(keys.rpcUrl);
+    this.getContracts = this.getContracts.bind(this);
+    this.startingCapitalContract = null;
   }
 
   componentWillMount = () => {
-     //constructing an instance of web 3 with preset config
-    const web3 = new Web3(keys.rpcUrl);
-    const contract = new web3.eth.Contract(abi, keys.address);
-    //call the methods to get the sct_sale_address value adn set state to re render ui
-    contract.methods.sct_sale_address().call().then(
-        address => this.setState({sctSaleAddress:address})
-      )
+    //getting contracts when app component starts
+    this.getContracts()
   }
 
-  setBalance(){
-    this.web3.bal().then(bal => {
-      this.setState({ balance : bal });
-    })
-  }
-
-  render () {
-    if (this.state.hasError) {
-      return <h1>Something went wrong.</h1>;
+  async getContracts() {
+    const salesDeployerContract = new this.web3.eth.Contract(abi, keys.address);
+    console.log(salesDeployerContract)
+    let nextContractAddress = await salesDeployerContract.methods.sct_sale_address().call();
+    console.log(nextContractAddress)
+    if (nextContractAddress) {
+      const startingCapitalContract = new this.web3.eth.Contract(startingCapitalAbi, nextContractAddress);
+      this.startingCapitalContract = startingCapitalContract;
+      this.setState({ loading: false, sctSaleAddress: nextContractAddress });
+    } else {
+      this.setState({ error: { exists: true, msg: 'No Address Found!' } })
     }
+  }
+  render() {
+    if (this.state.error.exists) {
+      return <h1 className="error-text">{this.state.error.msg}</h1>;
+    }
+
+    if (this.state.loading) {
+      return <h1 className="primary-text">Loading ... </h1>;
+    }
+
     return (
-      <div className="ContractWrapper">
-        SCT Sale Address : { this.state.sctSaleAddress }
+      <div className="contract-wrapper">
+        <div>
+          <h2>CAP Token</h2>
+        </div>
+        <div>
+          <b>SCT Sale Address</b> : {this.state.sctSaleAddress}
+        </div>
+        <div className="form">
+          {/* <TextField 
+            id="outlined-search" 
+            label="Amount" 
+            type="number" 
+            variant="outlined" />
+            <div className="buy">
+              <Button variant="contained">Buy</Button>
+            </div> */}
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-amount"
+              type={'text'}
+              //value={values.password}
+              //onChange={handleChange('password')}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    color="primary"
+                    aria-label="buy tokens"
+                    // onClick={handleClickShowPassword}
+                    // onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                    >
+                    $Buy
+                  </IconButton>
+                </InputAdornment>
+              }
+              labelWidth={80}
+            />
+          </FormControl>
+        </div>
       </div>
     );
   }
--- starting-capital-frontend/src/components/Contract/abi.js
+++ starting-capital-frontend/src/components/Contract/abi.js
@@ -1,56 +1,57 @@
-const abi = [
-    {
-        "inputs": [
-            {
-                "internalType": "string",
-                "name": "name",
-                "type": "string"
-            },
-            {
-                "internalType": "string",
-                "name": "symbol",
-                "type": "string"
-            },
-            {
-                "internalType": "address payable",
-                "name": "wallet",
-                "type": "address"
-            }
-        ],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "constructor"
-    },
-    {
-        "constant": true,
-        "inputs": [],
-        "name": "sct_sale_address",
-        "outputs": [
-            {
-                "internalType": "address",
-                "name": "",
-                "type": "address"
-            }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "constant": true,
-        "inputs": [],
-        "name": "token_address",
-        "outputs": [
-            {
-                "internalType": "address",
-                "name": "",
-                "type": "address"
-            }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-    }
-];
+const abi = 
+[
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "symbol",
+				"type": "string"
+			},
+			{
+				"internalType": "address payable",
+				"name": "wallet",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "sct_sale_address",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "token_address",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
 
 export default abi;
