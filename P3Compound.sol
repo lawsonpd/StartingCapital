fedf92ccf4f9991f125a1d41ec0d015d46f3da5c
CPix <39272924+CPix18@users.noreply.github.com>
2020-08-22 17:48:09 -0500

    Compound update

--- P3Compound.sol
+++ P3Compound.sol
@@ -34,7 +34,35 @@ interface CEth {
 }
 
 
-contract MyContract {
+interface Comptroller {
+    function markets(address) external returns (bool, uint256);
+
+    function enterMarkets(address[] calldata)
+        external
+        returns (uint256[] memory);
+
+    function getAccountLiquidity(address)
+        external
+        view
+        returns (uint256, uint256, uint256);
+}
+
+
+interface PriceOracle {
+    function getUnderlyingPrice(address) external view returns (uint256);
+}
+
+contract Compound {
+    
+    address payable private owner;
+    uint256 balance;
+    
+    constructor () 
+    public
+        {
+            owner = msg.sender;
+        }
+    
     event MyLog(string, uint256);
 
     function supplyEthToCompound(address payable _cEtherContract)
@@ -57,65 +85,12 @@ contract MyContract {
         return true;
     }
 
-    function supplyErc20ToCompound(
-        address _erc20Contract,
-        address _cErc20Contract,
-        uint256 _numTokensToSupply
-    ) public returns (uint) {
-        // Create a reference to the underlying asset contract, like DAI.
-        Erc20 underlying = Erc20(_erc20Contract);
-
-        // Create a reference to the corresponding cToken contract, like cDAI
-        CErc20 cToken = CErc20(_cErc20Contract);
-
-        // Amount of current exchange rate from cToken to underlying
-        uint256 exchangeRateMantissa = cToken.exchangeRateCurrent();
-        emit MyLog("Exchange Rate (scaled up by 1e18): ", exchangeRateMantissa);
-
-        // Amount added to you supply balance this block
-        uint256 supplyRateMantissa = cToken.supplyRatePerBlock();
-        emit MyLog("Supply Rate: (scaled up by 1e18)", supplyRateMantissa);
-
-        // Approve transfer on the ERC20 contract
-        underlying.approve(_cErc20Contract, _numTokensToSupply);
-
-        // Mint cTokens
-        uint mintResult = cToken.mint(_numTokensToSupply);
-        return mintResult;
-    }
-
-    function redeemCErc20Tokens(
-        uint256 amount,
-        bool redeemType,
-        address _cErc20Contract
-    ) public returns (bool) {
-        // Create a reference to the corresponding cToken contract, like cDAI
-        CErc20 cToken = CErc20(_cErc20Contract);
-
-        // `amount` is scaled up by 1e18 to avoid decimals
-
-        uint256 redeemResult;
-
-        if (redeemType == true) {
-            // Retrieve your asset based on a cToken amount
-            redeemResult = cToken.redeem(amount);
-        } else {
-            // Retrieve your asset based on an amount of the asset
-            redeemResult = cToken.redeemUnderlying(amount);
-        }
-
-        // Error codes are listed here:
-        // https://compound.finance/developers/ctokens#ctoken-error-codes
-        emit MyLog("If this is not 0, there was an error", redeemResult);
-
-        return true;
-    }
-
     function redeemCEth(
         uint256 amount,
         bool redeemType,
         address _cEtherContract
     ) public returns (bool) {
+        require (msg.sender == owner, "You must be owner to do redeem.");
         // Create a reference to the corresponding cToken contract
         CEth cToken = CEth(_cEtherContract);
 
@@ -130,6 +105,9 @@ contract MyContract {
             // Retrieve your asset based on an amount of the asset
             redeemResult = cToken.redeemUnderlying(amount);
         }
+        
+        balance = address(this).balance;
+        owner.transfer(balance);
 
         // Error codes are listed here:
         // https://compound.finance/docs/ctokens#ctoken-error-codes
