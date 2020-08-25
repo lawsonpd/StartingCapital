b7297417bc216a1975e36c7e3f8513cf2e54a62f
CPix <39272924+CPix18@users.noreply.github.com>
2020-08-24 23:07:48 -0500

    Frontend description

--- starting-capital-frontend/src/components/Main/Main.jsx
+++ starting-capital-frontend/src/components/Main/Main.jsx
@@ -22,7 +22,7 @@ class Main extends PureComponent {
     }
     return (
       <Container maxWidth="md" className="app-container">
-        Possible description explaining what this app is about and some instructions?
+        Thanks for participating in Harold's Crowdsale! Below you will find a box to input the amount to contribute, a box where you select "wei" or "ether" - select "ether", and another where you input an Ethereum address you own the private keys for (beneficiary address). The Ethereum address you input will be the location at which you receive your Starting Capital ERC20 tokens and can be easily accessed through the Metamask extension on your browser. Limt 1 "ether" per participant.
         <Contract />
       </Container>
     );
