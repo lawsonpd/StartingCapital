50f711c9e5d3dd82a50d3fa7ae7ef90ae7807963
CPix <39272924+CPix18@users.noreply.github.com>
2020-08-25 19:12:43 -0500

    update

--- P3CompoundDev.sol
+++ P3CompoundDev.sol
@@ -237,7 +237,6 @@ contract Compound {
 }
 
 contract CrowdsaleGiveBack {
-    using SafeMath for uint;
     address payable crowdsale_one;
     address payable crowdsale_two;
     address payable crowdsale_three;
@@ -248,8 +247,7 @@ contract CrowdsaleGiveBack {
     address payable crowdsale_eight;
     address payable crowdsale_nine;
     address payable crowdsale_ten;
-    uint send_amount;
-    //constructor
+    
     constructor(address payable _one, address payable _two, address payable _three, 
         address payable _four, address payable _five, address payable _six,
         address payable _seven, address payable _eight, address payable _nine,
@@ -265,27 +263,23 @@ contract CrowdsaleGiveBack {
             crowdsale_nine = _nine;
             crowdsale_ten = _ten;
     }
-    function balance() public view returns(uint) {
-        return address(this).balance;
-    }
-    function deposit() public payable {
-        uint tmp_amt = msg.value;
-        uint amount = tmp_amt.div(10);
-        crowdsale_one.call.value(amount);
-        crowdsale_two.call.value(amount);
-        crowdsale_three.call.value(amount);
-        crowdsale_four.call.value(amount);
-        crowdsale_five.call.value(amount);
-        crowdsale_six.call.value(amount);
-        crowdsale_seven.call.value(amount);
-        crowdsale_eight.call.value(amount);
-        crowdsale_nine.call.value(amount);
-        crowdsale_ten.call.value(amount);
-        uint remainder = tmp_amt.sub(amount);
-        remainder = remainder.mul(10);
-        msg.sender.call.value(remainder);
+
+    function distribute() public payable {
+        uint amount = msg.value / 10;
+        crowdsale_one.transfer(amount);
+        crowdsale_two.transfer(amount);
+        crowdsale_three.transfer(amount);
+        crowdsale_four.transfer(amount);
+        crowdsale_five.transfer(amount);
+        crowdsale_six.transfer(amount);
+        crowdsale_seven.transfer(amount);
+        crowdsale_eight.transfer(amount);
+        crowdsale_nine.transfer(amount);
+        crowdsale_ten.transfer(amount);
+        uint remainder = msg.value - amount * 10;
+        msg.sender.transfer(remainder);
     }
     function() external payable {
-        deposit();
+        distribute();
     }
 }
--- tokenized-savings-frontend/packages/react-app/src/hooks/index.js
+++
@@ -1,6 +0,0 @@
-export { default as useContractLoader } from "./ContractLoader";
-export { default as useCustomContractLoader } from "./CustomContractLoader";
-export { default as useContractExistsAtAddress } from "./ContractExistsAtAddress";
-export { default as useExchangePrice } from "./ExchangePrice";
-export { default as useGasPrice } from "./GasPrice";
-export { default as useUserProvider } from "./UserProvider";
