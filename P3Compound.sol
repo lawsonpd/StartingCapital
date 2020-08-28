86ec215361837108b39c4aa63cbf1a5527e356ca
Peter Lawson <lawsonpd@pm.me>
2020-08-27 20:08:12 -0500

    Combine P3Compound, P3TokenMintable and P3Crowdsale.

--- P3Compound.sol
+++ P3Compound.sol
@@ -2,6 +2,58 @@ pragma solidity ^0.5.12;
 
 import "github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/math/SafeMath.sol";
 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
+import "./P3TokenMintable.sol";
+
+
+contract StartingCapital is Crowdsale, MintedCrowdsale {
+    
+    constructor (uint rate, address payable wallet, Capital token) 
+        Crowdsale(rate, wallet, token)
+        public
+        {
+            //constructor can stay empty
+        }
+    
+}
+
+contract CapitalTokenSaleDeployer {
+    
+    address public sct_sale_address;
+    address public token_address;
+    
+    constructor (string memory name, string memory symbol, 
+        address payable wallet) public {
+            
+            Capital token = new Capital(name, symbol, 100);
+            token_address = address(token);
+            
+            StartingCapital sct_sale = new StartingCapital(1000000000000000000, wallet, token); //1 token = 1 eth
+            sct_sale_address = address(sct_sale);
+            
+            token.addMinter(sct_sale_address);
+            token.renounceMinter();
+            
+        }
+
+
+
+contract Capital is ERC20, ERC20Detailed, ERC20Mintable {
+    
+    constructor (string memory name, string memory symbol, uint initial_supply)
+        ERC20Detailed (name, symbol, 18)
+        public
+        {
+            //constructor can stay empty
+        }
+
+}
+
 
 interface Erc20 {
     function approve(address, uint256) external returns (bool);
