0eaaaa7297cb596ad18e572a78a9867c2f058e9c
Peter Lawson <lawsonpd@pm.me>
2020-08-27 20:09:45 -0500

    Fix url.

--- P3Compound.sol
+++ P3Compound.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.12;
 
-import "github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/math/SafeMath.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/math/SafeMath.sol";
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
